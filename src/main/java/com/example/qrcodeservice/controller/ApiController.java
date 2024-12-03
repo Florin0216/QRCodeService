cd4370eb7448309224cb20fe3a83d523c7a7647b
Florin0216 <florin.barbu16@yahoo.com>
2024-12-03 15:15:55 +0200

    Update

--- src/main/java/com/example/qrcodeservice/controller/ApiController.java
+++ src/main/java/com/example/qrcodeservice/controller/ApiController.java
@@ -10,14 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Map;
 import java.awt.image.BufferedImage;
+
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    private QRCodeService qrCodeService;
+    private final QRCodeService qrCodeService;
 
     public ApiController() {
         this.qrCodeService = new QRCodeService();
@@ -25,20 +27,20 @@ public class ApiController {
 
     @GetMapping("/health")
     public ResponseEntity getHealth() {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @GetMapping("/qrcode")
     public ResponseEntity<?> getImage(@RequestParam(name = "contents") String contents,
                                       @RequestParam(name = "correction", defaultValue = "L", required = false) String correction,
-                                      @RequestParam(name = "size" , defaultValue = "250", required = false) int size,
+                                      @RequestParam(name = "size", defaultValue = "250", required = false) int size,
                                       @RequestParam(name = "type", defaultValue = "png", required = false) String type) throws WriterException {
-        Map<String, String> validationError = ValidationUtil.validateInputs(contents,correction, size, type);
+        Map<String, String> validationError = ValidationUtil.validateInputs(contents, correction, size, type);
         if (validationError != null) {
             return ResponseEntity.badRequest().body(validationError);
         }
 
-        BufferedImage bufferedImage = qrCodeService.generateQRCode(contents,correction, size);
+        BufferedImage bufferedImage = qrCodeService.generateQRCode(contents, correction, size);
 
         MediaType mediaType = ValidationUtil.validatePhotoType(type);
 
--- src/main/java/com/example/qrcodeservice/utils/ValidationUtil.java
+++ src/main/java/com/example/qrcodeservice/utils/ValidationUtil.java
@@ -1,17 +1,20 @@
 package com.example.qrcodeservice.utils;
 
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.http.MediaType;
+
 import java.util.Map;
 
 public class ValidationUtil {
-    public static Map<String, String> validateInputs(String contents,String correction, int size, String type) {
+    public static Map<String, String> validateInputs(String contents, String correction, int size, String type) {
         if (contents == null || contents.trim().isEmpty()) {
             return Map.of("error", "Contents cannot be null or blank");
         }
         if (size < 150 || size > 350) {
             return Map.of("error", "Image size must be between 150 and 350 pixels");
         }
-        if(!correction.equals("L") && !correction.equals("M") && !correction.equals("Q") && !correction.equals("H")) {
+        if (!correction.equals("L") && !correction.equals("M") && !correction.equals("Q") && !correction.equals("H")) {
             return Map.of("error", "Permitted error correction levels are L, M, Q, H");
         }
         if (!type.equals("png") && !type.equals("jpeg") && !type.equals("gif")) {
@@ -21,12 +24,21 @@ public class ValidationUtil {
     }
 
     public static MediaType validatePhotoType(String type) {
-        MediaType mediaType = switch (type) {
+        return switch (type) {
             case "png" -> MediaType.IMAGE_PNG;
             case "jpeg" -> MediaType.IMAGE_JPEG;
             case "gif" -> MediaType.IMAGE_GIF;
             default -> null;
         };
-        return mediaType;
+    }
+
+    public static Map<EncodeHintType, ?> validateCorrection(String correction) {
+        return switch (correction) {
+            case "L" -> Map.of(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            case "M" -> Map.of(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+            case "Q" -> Map.of(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.Q);
+            case "H" -> Map.of(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            default -> null;
+        };
     }
 }
